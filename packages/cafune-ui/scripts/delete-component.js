3e5ec44c214a925a9f790dab933cfe320177c3cf
evont <evontgoh@foxmail.com>
2019-06-18 15:15:54 +0800

    chore: new way to create component & add delete component func

---
+++ packages/cafune-ui/scripts/delete-component.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const styleRoot = path.resolve(process.cwd(), './style');
+const compRoot = path.resolve(process.cwd(), './components');
+const storiesRoot = path.resolve(process.cwd(), './stories');
+const testRoot = path.resolve(process.cwd(), './__test__/components');
+
+const { getComponentName } = require('./util/comp');
+
+function main() {
+  const args = process.argv;
+  const name = args[2];
+  if (name === undefined) {
+    console.error('Component name is required');
+    process.exit();
+  }
+  const compName = getComponentName(name);
+  // remove(compName);
+  deleteMapping(compName);
+  deleteEnty(name);
+  deleteComp(name);
+  deleteTestSuit(name);
+  deleteStories(name);
+  deleteScss(name);
+  console.log('delete completed');
+}
+
+main();
+
+// 删除mapping 中的项目
+function deleteMapping(compName) {
+  const mappingJsonPath = path.resolve(compRoot, 'mapping.json');
+  if (fs.existsSync(mappingJsonPath)) {
+    const mappingJsonFile = fs.readFileSync(mappingJsonPath, 'utf-8');
+    const mappingJson = JSON.parse(mappingJsonFile);
+    if (mappingJson[compName]) {
+      delete mappingJson[compName];
+      fs.writeFileSync(mappingJsonPath, JSON.stringify(mappingJson, null, 2));
+    }
+  }
+}
+// 删除入口文件
+function deleteEnty(name) {
+  const entryPath = path.resolve(compRoot, 'index.js');
+  if (fs.existsSync(entryPath)) {
+    const entryFile = fs.readFileSync(entryPath, 'utf-8');
+    const modExports = entryFile.trim().split('\n') || [];
+    const exportIndex = modExports.indexOf(`export * from './${name}';`);
+    if (exportIndex !== -1) {
+      modExports.splice(exportIndex, 1);
+      fs.writeFileSync(entryPath, `${modExports.join('\n')}\n`);
+    }
+  }
+}
+
+// 递归删除文件，如果是文件夹，因其包含文件无法直接unlink，需要递归删除
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path)) {
+    if (fs.lstatSync(path).isDirectory()) {
+      fs.readdirSync(path).forEach(file => {
+        var curPath = path + '/' + file;
+        if (fs.lstatSync(curPath).isDirectory()) {
+          // recurse
+          deleteFolderRecursive(curPath);
+        } else {
+          // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    } else {
+      fs.unlinkSync(path);
+    }
+  }
+}
+
+// 删除组件的目录
+function deleteComp(name) {
+  const compDir = `${compRoot}/${name}`;
+  deleteFolderRecursive(compDir);
+}
+
+// 删除组件的css
+function deleteScss(name) {
+  deleteFolderRecursive(`${styleRoot}/${name}.scss`);
+  const entryPath = `${styleRoot}/index.scss`;
+  if (fs.existsSync(entryPath)) {
+    const entryFile = fs.readFileSync(entryPath, 'utf-8');
+    const modExports = entryFile.trim().split('\n') || [];
+    const exportIndex = modExports.indexOf(`@import './${name}';`);
+    if (exportIndex !== -1) {
+      modExports.splice(exportIndex, 1);
+      fs.writeFileSync(entryPath, `${modExports.join('\n')}\n`);
+    }
+  }
+}
+
+// 删除stories 下的js 和md
+function deleteStories(name) {
+  const stories = `${storiesRoot}/${name}.stories.js`;
+  deleteFolderRecursive(stories);
+  const storiesMd = `${storiesRoot}/markdown/${name}.md`;
+  deleteFolderRecursive(storiesMd);
+}
+
+// 删除测试用例
+function deleteTestSuit(name) {
+  const testCompDir = `${testRoot}/${name}`;
+  deleteFolderRecursive(testCompDir);
+}
--- packages/cafune-ui/scripts/new-component.js
+++ packages/cafune-ui/scripts/new-component.js
@@ -1,107 +1,56 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 const path = require('path');
+const { getComponentName, sortByModulePath } = require('./util/comp');
 const styleRoot = path.resolve(process.cwd(), './style');
 const compRoot = path.resolve(process.cwd(), './components');
 const storiesRoot = path.resolve(process.cwd(), './stories');
 const testRoot = path.resolve(process.cwd(), './__test__/components');
-function main() {
-  const args = process.argv;
-  const compName = args[2];
-  if (compName === undefined) {
-    console.error('Component name is required');
-    process.exit();
-  }
-
-  addFiles(compName);
-}
-
-main();
-
-function sortByModulePath(lines) {
-  lines.sort((a, b) => {
-    const modPathRegexp = /'.+'/;
-    const modA = modPathRegexp.exec(a)[0];
-    const modB = modPathRegexp.exec(b)[0];
-
-    return modA.localeCompare(modB);
-  });
-
-  return lines;
-}
-
-function firstAlphabetUpper(str) {
-  return str.substring(0, 1).toUpperCase() + str.substring(1);
-}
-
-function getComponentName(name) {
-  let componentName;
-  if (name.includes('-')) {
-    componentName = name
-      .split('-')
-      .map(item => {
-        return firstAlphabetUpper(item);
-      })
-      .join('');
-  } else {
-    componentName = firstAlphabetUpper(name);
-  }
-  return componentName;
-}
-
-function addFiles(name) {
-  const entryPath = path.resolve(compRoot, 'index.js');
-  let escapePush = false;
-  if (!fs.existsSync(entryPath)) {
-    fs.writeFileSync(entryPath, `export * from './${name}';`);
-    escapePush = true;
-  }
-  const entryFile = fs.readFileSync(entryPath, 'utf-8');
-  const modExports = entryFile.trim().split('\n') || [];
-  const compDir = `${compRoot}/${name}`;
-  const compName = getComponentName(name);
-  if (!fs.existsSync(compDir)) {
-    fs.mkdirSync(compDir);
-    // add to entry, in order to use component by tying 'import { comp } from 'components';
-    console.log(`Adding new components: ${compName} to index.js`);
-    !escapePush && modExports.push(`export * from './${name}';`);
-    sortByModulePath(modExports);
-    fs.writeFileSync(entryPath, `${modExports.join('\n')}\n`);
-    // add to mapping, for code splitting;
-    console.log(`Adding new components: ${compName} to mapping.json`);
-    const mappingJsonPath = path.resolve(compRoot, 'mapping.json');
-    if (!fs.existsSync(mappingJsonPath)) {
-      fs.writeFileSync(mappingJsonPath, '{}');
+console.clear();
+const inquirer = require('inquirer');
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: `What is the component's name (in CamelCase or kebab-case)`,
+    validate(value) {
+      if (value === '') {
+        return 'Component name is required';
+      }
+      const compDir = `${compRoot}/${value}`;
+      if (fs.existsSync(compDir)) {
+        return `${value} already exists, please choose another name.`;
+      }
+      return true;
     }
-    const mappingJsonFile = fs.readFileSync(mappingJsonPath, 'utf-8');
-    const mappingJson = JSON.parse(mappingJsonFile);
-    mappingJson[compName] = {
-      js: `components/${name}`,
-      css: [name]
-    };
-    fs.writeFileSync(mappingJsonPath, JSON.stringify(mappingJson, null, 2));
-  } else {
-    console.log(`${compName} already exists, please choose another name.`);
-    process.exit(2);
+  },
+  {
+    type: 'confirm',
+    name: 'isHasStyle',
+    message: 'Would this component have a style.scss? ',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'isHasStories',
+    message: 'Would this component have a stories.js? ',
+    default: true
   }
-  // create js & css to component folder
-  fs.writeFileSync(
-    `${compDir}/index.js`,
-    `import { Component } from 'preact';\n\nexport class ${compName} extends Component {\n  render() {\n    return <div />;\n  }\n}\nexport default ${compName};\n`
-  );
-  fs.writeFileSync(`${styleRoot}/${name}.scss`, '')
-  const entryCssPath = `${styleRoot}/index.scss`;
-  escapePush = false;
-  if (!fs.existsSync(entryCssPath)) {
-    fs.writeFileSync(entryCssPath, `@import './${name}';`);
-    escapePush = true;
-  }
-  const entryCss = fs.readFileSync(entryCssPath, 'utf-8');
-  const styleImports = entryCss.trim().split('\n') || [];
-  !escapePush && styleImports.push(`@import './${name}';`);
-  fs.writeFileSync(entryCssPath, `${styleImports.join('\n')}\n`);
+];
 
+inquirer.prompt(questions).then(answers => {
+  const { name, isHasStyle, isHasStories } = answers;
+  const compName = getComponentName(name);
+  writeMapping(compName, name);
+  writeEntry(compName, name);
+  writeComp(compName, name, isHasStyle);
+  if (isHasStories) {
+    writeStories(compName, name);
+  }
+  writeTestSuit(compName, name);
+});
+
+function writeTestSuit(compName, name) {
   const testCompDir = `${testRoot}/${name}`;
   if (!fs.existsSync(testCompDir)) {
     fs.mkdirSync(testCompDir);
@@ -112,40 +61,94 @@ function addFiles(name) {
   fs.writeFileSync(
     `${testCompDir}/index.test.js`,
     `
-import { ${compName} } from 'components';
-import { render } from 'enzyme';
-import { shallow, deep } from 'preact-render-spy';
-  `
+  import { ${compName} } from 'components';
+  import { render } from 'enzyme';
+  import { shallow, deep } from 'preact-render-spy';
+    `
   );
-  // create stories to write components doc
+}
+
+function writeStories(compName, name) {
   fs.writeFileSync(
     `${storiesRoot}/${name}.stories.js`,
     `
-import { storiesOf } from '@storybook/preact';
-import { ${compName} } from 'components';
-import './style.css';
-import 'style/${name}.css';
-import notes from './markdown/${name}.md';
+  import { storiesOf } from '@storybook/preact';
+  import { ${compName} } from 'components';
+  import './style.css';
+  import 'style/${name}.css';
+  import notes from './markdown/${name}.md';
 
-storiesOf('Component|${compName}', module).add(
-  '${compName}',
-  () => (),
-  {
-    notes
-  }
-);
+  storiesOf('Component|${compName}', module).add(
+    '${compName}',
+    () => (),
+    {
+      notes
+    }
+  );
     `
   );
   fs.writeFileSync(
     `${storiesRoot}/markdown/${name}.md`,
     `
-## 引入
-import { ${compName} } from 'components';
-## 使用
+  ## 引入
+  import { ${compName} } from 'components';
+  ## 使用
 
-## 配置项
-| 参数 | 说明 | 类型 | 默认值 |备选值 | 是否必须 |
-| --- | --- | --- | --- | --- | --- |
+  ## 配置项
+  | 参数 | 说明 | 类型 | 默认值 |备选值 | 是否必须 |
+  | --- | --- | --- | --- | --- | --- |
     `
   );
+}
+
+function writeMapping(compName, name) {
+  // add to mapping, for code splitting;
+  console.log(`Adding new components: ${compName} to mapping.json`);
+  const mappingJsonPath = path.resolve(compRoot, 'mapping.json');
+  if (!fs.existsSync(mappingJsonPath)) {
+    fs.writeFileSync(mappingJsonPath, '{}');
+  }
+  const mappingJsonFile = fs.readFileSync(mappingJsonPath, 'utf-8');
+  const mappingJson = JSON.parse(mappingJsonFile);
+  mappingJson[compName] = {
+    js: `components/${name}`,
+    css: [name]
+  };
+  fs.writeFileSync(mappingJsonPath, JSON.stringify(mappingJson, null, 2));
+}
+
+function writeEntry(compName, name) {
+  const entryPath = path.resolve(compRoot, 'index.js');
+  if (!fs.existsSync(entryPath)) {
+    fs.writeFileSync(entryPath, '');
+  }
+  const entryFile = fs.readFileSync(entryPath, 'utf-8');
+  const modExports = entryFile.trim().split('\n') || [];
+  const compDir = `${compRoot}/${name}`;
+  fs.mkdirSync(compDir);
+  // add to entry, in order to use component by tying 'import { comp } from 'components';
+  console.log(`Adding new components: ${compName} to index.js`);
+  modExports.push(`export * from './${name}';`);
+  sortByModulePath(modExports);
+  fs.writeFileSync(entryPath, `${modExports.join('\n')}\n`);
+}
+
+function writeComp(compName, name, isHasStyle) {
+  const compDir = `${compRoot}/${name}`;
+  // create js & css to component folder
+  fs.writeFileSync(
+    `${compDir}/index.js`,
+    `import { Component } from 'preact';\n\nexport class ${compName} extends Component {\n  render() {\n    return <div />;\n  }\n}\nexport default ${compName};\n`
+  );
+  if (isHasStyle) {
+    fs.writeFileSync(`${styleRoot}/${name}.scss`, '')
+    const entryCssPath = `${styleRoot}/index.scss`;
+    if (!fs.existsSync(entryCssPath)) {
+      fs.writeFileSync(entryCssPath, '');
+      const entryCss = fs.readFileSync(entryCssPath, 'utf-8');
+      const styleImports = entryCss.trim().split('\n') || [];
+      styleImports.push(`@import './${name}';`);
+      fs.writeFileSync(entryCssPath, `${styleImports.join('\n')}\n`);
+    }
+  }
 }
